d5c3d72bc74fac6e6d77a8bcd292636cf62f7000
Santos de Jesús Mateo Camacho <smateo@epica.solutions>
2024-12-16 19:20:09 -0700

    end of section 2

---
+++ Infrastructure/Config/ProductConfiguration.cs
@@ -0,0 +1,15 @@
+using System;
+using Core.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace Infrastructure.Config;
+
+public class ProductConfiguration : IEntityTypeConfiguration<Product>
+{
+    public void Configure(EntityTypeBuilder<Product> builder)
+    {
+        builder.Property(p => p.Name).IsRequired().HasMaxLength(100);
+        builder.Property(p => p.Price).IsRequired().HasColumnType("decimal(18, 2)");
+    }
+}
